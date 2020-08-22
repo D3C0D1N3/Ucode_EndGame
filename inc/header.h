d84e990f652e7965e22dbd48d6ec10786e716beb
D3C0D1N3 <ymrozek@student.ucode.world>
2020-08-21 21:23:35 -0400

    added some for working home

--- inc/header.h
+++ inc/header.h
@@ -1,8 +1,12 @@
 #pragma once
 
-//наши фреймворки
-#include "../frameworks/SDL2.framework/Headers/SDL.h"
-#include "../frameworks/SDL2_image.framework/Headers/SDL_image.h"
+//наши фреймворки (Тут будут закомичены хедеры и левые фреймворки)
+//Все что в include/SDL... для линукса на дому.
+//#include "../macFrameworks/SDL2.framework/Headers/SDL.h"
+//#include "../macFrameworks/SDL2_image.framework/Headers/SDL_image.h"
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+#include <SDL2/SDL2_gfxPrimitives.h>
 
 //Подключение хэдеров
 
@@ -13,8 +17,6 @@
 #include <stdbool.h>
 #include <unistd.h>
 #include <string.h>
-
-
 
 //декларация функций
 void init_sdl();
--- src/event.c
+++ src/event.c
@@ -9,7 +9,7 @@ void render_update(SDL_Renderer *rend, SDL_Texture *text[], SDL_Rect *destrect[]
 void event(SDL_Window *win, SDL_Renderer *rend) {
     bool game = true;
     float x_vel = 1.2;
-    float y_vel = 1.2;
+    float y_vel = 1;
 
     SDL_DisplayMode display;
                     display.w = 480;
@@ -26,9 +26,11 @@ void event(SDL_Window *win, SDL_Renderer *rend) {
                   back_rect.y = 0;
                   back_rect.w = display.w;
                   back_rect.h = display.h;
-    SDL_Texture *hero_tex = IMG_LoadTexture(rend, "/Users/ymrozek/Desktop/EndGameCollab/assets/hero.png");
-    SDL_Texture *back_tex = IMG_LoadTexture(rend, "/Users/ymrozek/Desktop/EndGameCollab/assets/images.jpg");
-    
+    //SDL_Texture *hero_tex = IMG_LoadTexture(rend, "/Users/ymrozek/Desktop/EndGameCollab/assets/hero.png");
+    //SDL_Texture *back_tex = IMG_LoadTexture(rend, "/Users/ymrozek/Desktop/EndGameCollab/assets/images.jpg");
+    SDL_Texture *hero_tex = IMG_LoadTexture(rend, "/root/Desktop/EndGameCollab/assets/hero/hero.png");
+    SDL_Texture *back_tex = IMG_LoadTexture(rend, "/root/Desktop/EndGameCollab/assets/level/images.jpg");
+
     SDL_Texture *ARRAY_Textures[2] = {back_tex, hero_tex};
     SDL_Rect *ARRAY_Rect[2] = {&back_rect, &hero_rect};
     int ARRAY_texturesState[2] = {1, 1};
@@ -36,34 +38,37 @@ void event(SDL_Window *win, SDL_Renderer *rend) {
     //EVENT LOOOOOOP
     while(game) {
         const Uint8 *keyboardState = SDL_GetKeyboardState(NULL);
-        while(SDL_PollEvent(&event)) {
-            if(event.type == SDL_QUIT)
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_QUIT)
 	            game = false;
             }
-                if (keyboardState[SDL_SCANCODE_W]) {
-                    hero_rect.y -= y_vel;
-                    SDL_RenderClear(rend);
-                    SDL_RenderCopy(rend, hero_tex, NULL, &hero_rect);
-                }
-                if (keyboardState[SDL_SCANCODE_S]) {
-                    hero_rect.y += y_vel;
-                    SDL_RenderClear(rend);
-                    SDL_RenderCopy(rend, hero_tex, NULL, &hero_rect);
-                }
-                if (keyboardState[SDL_SCANCODE_A]) {
-                    hero_rect.x -= x_vel;
-                    SDL_RenderClear(rend);
-                    SDL_RenderCopy(rend, hero_tex, NULL, &hero_rect);
-                }
-                if (keyboardState[SDL_SCANCODE_D]) {
-                    hero_rect.x += x_vel;
-                    SDL_RenderClear(rend);
-                    SDL_RenderCopy(rend, hero_tex, NULL, &hero_rect);
-                }
-            render_update(rend, ARRAY_Textures, ARRAY_Rect, ARRAY_texturesState);
-            SDL_RenderPresent(rend);
+            if (keyboardState[SDL_SCANCODE_W]) {
+                hero_rect.y -= y_vel;
+                SDL_RenderClear(rend);
+                SDL_RenderCopy(rend, hero_tex, NULL, &hero_rect);
+            }
+            if (keyboardState[SDL_SCANCODE_S]) {
+                hero_rect.y += y_vel;
+                SDL_RenderClear(rend);
+                SDL_RenderCopy(rend, hero_tex, NULL, &hero_rect);
+            }
+            if (keyboardState[SDL_SCANCODE_A]) {
+                hero_rect.x -= x_vel;
+                SDL_RenderClear(rend);
+                SDL_RenderCopy(rend, hero_tex, NULL, &hero_rect);
+            }
+            if (keyboardState[SDL_SCANCODE_D]) {
+                hero_rect.x += x_vel * 2;
+                SDL_RenderClear(rend);
+                SDL_RenderCopy(rend, hero_tex, NULL, &hero_rect);
+            }
+        render_update(rend, ARRAY_Textures, ARRAY_Rect, ARRAY_texturesState);
+        SDL_RenderPresent(rend);
         if (hero_rect.x <= 0) hero_rect.x += 2;
-        if (hero_rect.x >= 440) hero_rect.x -= 2; 
+        if (hero_rect.x >= 440) hero_rect.x -= 2;
+        if (hero_rect.y <= 240) hero_rect.y += 2;
+        if (hero_rect.y >= 260) hero_rect.y -= 2; 
+
     }
 
     SDL_RenderClear(rend);
